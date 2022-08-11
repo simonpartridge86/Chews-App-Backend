010920a3bb38f13d444464e88794cda21614b4e8
simonpartridge86 <simonpartridge86@gmail.com>
2022-08-12 00:26:29 +0100

    hidden API key in .env file

--- models/getRandomMeal.js
+++ models/getRandomMeal.js
@@ -1,5 +1,8 @@
 import fetch from "node-fetch";
 import { formatResults } from "./formatResults.js";
+import dotenv from "dotenv";
+dotenv.config();
+const API_URL = process.env.MEALDB_URL;
 
 //GET RANDOM MEAL
 
@@ -21,16 +24,14 @@ export async function getRandomMeal(category) {
 async function getRandomMainMeal() {
   //First fetch to get list of results:
   const result = await Promise.all([
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Seafood"),
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Beef"),
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Chicken"),
-    fetch(
-      "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Vegetarian"
-    ),
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Goat"),
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Lamb"),
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Pasta"),
-    fetch("https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Vegan"),
+    fetch(`${API_URL}filter.php?c=Seafood`),
+    fetch(`${API_URL}filter.php?c=Beef`),
+    fetch(`${API_URL}filter.php?c=Chicken`),
+    fetch(`${API_URL}filter.php?c=Vegetarian`),
+    fetch(`${API_URL}filter.php?c=Goat`),
+    fetch(`${API_URL}filter.php?c=Lamb`),
+    fetch(`${API_URL}filter.php?c=Pasta`),
+    fetch(`${API_URL}filter.php?c=Vegan`),
   ]);
 
   let promises = [];
@@ -48,9 +49,7 @@ async function getRandomMainMeal() {
 
     //Second fetch to get individual random result full recipe:
     const finalRandomRecipe = await (
-      await fetch(
-        `https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${randomRecipe}`
-      )
+      await fetch(`${API_URL}lookup.php?i=${randomRecipe}`)
     ).json();
 
     const selectedMeal = finalRandomRecipe.meals[0];
@@ -62,11 +61,7 @@ async function getRandomMainMeal() {
 
 //GET RANDOM BREAKFAST
 async function getRandomBreakfast() {
-  const result = await (
-    await fetch(
-      "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Breakfast"
-    )
-  ).json();
+  const result = await (await fetch(`${API_URL}filter.php?c=Breakfast`)).json();
   if (result.length === 0) {
     return [];
   } else {
@@ -75,9 +70,7 @@ async function getRandomBreakfast() {
     //Second fetch to get individual random result full breakfast recipe:
     const randomBreakfastId = result.meals[randomResult].idMeal;
     const finalRandomRecipe = await (
-      await fetch(
-        `https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${randomBreakfastId}`
-      )
+      await fetch(`${API_URL}lookup.php?i=${randomBreakfastId}`)
     ).json();
 
     const selectedMeal = finalRandomRecipe.meals[0];
@@ -87,11 +80,7 @@ async function getRandomBreakfast() {
 
 //GET RANDOM DESSERT
 async function getRandomDessert() {
-  const result = await (
-    await fetch(
-      "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=Dessert"
-    )
-  ).json();
+  const result = await (await fetch(`${API_URL}filter.php?c=Dessert`)).json();
   if (result.length === 0) {
     return [];
   } else {
@@ -100,9 +89,7 @@ async function getRandomDessert() {
     //Second fetch to get individual random result full dessert recipe:
     const randomDessertId = result.meals[randomResult].idMeal;
     const finalRandomRecipe = await (
-      await fetch(
-        `https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${randomDessertId}`
-      )
+      await fetch(`${API_URL}lookup.php?i=${randomDessertId}`)
     ).json();
 
     const selectedMeal = finalRandomRecipe.meals[0];
