61e55f4ce268ff44e2620976622da63702772a6c
samsonhumber <printerfissh693@gmail.com>
2022-08-19 15:25:07 +0100

    supertest tested that ingredients list works

--- router/routes.test.js
+++ router/routes.test.js
@@ -1,7 +1,3 @@
-/*import express from 'express';
-import { request } from 'supertest';
-import { test, expect } from "@jest/globals";
-*/
 import request from 'supertest';
 import { test, expect } from "@jest/globals";
 import app from '../app.js';
@@ -11,10 +7,22 @@ test('See if the app loads at all', async () => {
     app.get('/user', function(req, res) {
        res.status(200).json({ name: 'john' });
     }); 
+
     await request(app)
     .get('/user')
     .expect('Content-Type', /json/)
     .expect('Content-Length', '15')
+    .expect(200);
+});
+
+test('See if get ingredients route for cucumber returns a Cucumber', async () => {
+    app.get('/ingredients-list/cucumber', function(req, res) {
+       res.status(200);
+    }); 
+    
+    await request(app)
+    .get('/ingredients-list/cucumber')
+    .expect({success: true, payload: ["Cucumber",]})
     .expect(200);
 })
 
