18349b89d37f0692b2db00bcd8ed4fe0fecf47cc
Mino Devito <mino.devito@gmail.com>
2022-08-19 14:51:43 +0100

    random meal test added

--- models/models.test.js
+++ models/models.test.js
@@ -1,6 +1,6 @@
 import { getIngredients } from './getIngredients.js';
 import { test,expect } from '@jest/globals';
-
+import { getRandomMeal } from './getRandomMeal';
 
 describe ('Ingredient search tests', () => {
 test('expects ingredient search for cucumber to return an array containing cucumber', async () => {
@@ -31,3 +31,32 @@ test('expects ingredient search for plastics to return an empty array', async ()
 })
 });
 
+describe ('Random meal tests', () => {
+    test('expects random meals to be random meals', async () => {
+        //ARRANGE
+        const actual = await getRandomMeal('main');
+        //ACT
+        const expected = expect.any(Array);
+        //ASSERT
+        expect(actual).toEqual(expected);
+        });
+
+    test('expects random meals to be random meals', async () => {
+        //ARRANGE
+        const actual = await getRandomMeal('main');
+        //ACT
+        const expected = expect.any(String);
+        //ASSERT
+        expect(actual[0].id).toEqual(expected);
+        });
+
+    test('expects random meals to be random meals', async () => {
+        //ARRANGE
+        const actual = await getRandomMeal('laptop');
+        //ACT
+        const expected = undefined;
+        //ASSERT
+        expect(actual).toEqual(expected);
+    });
+    
+    });
